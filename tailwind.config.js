2bce9e075e42aec35939ba74b2c05d20f54d5bd1
Azmi Azis <azmiazis708@gmail.com>
2026-01-14 11:02:26 +0700

    hampir final_azmi

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    "Plus Jakarta Sans",
+                    "ui-sans-serif",
+                    "system-ui",
+                    "-apple-system",
+                    "Segoe UI",
+                    "Roboto",
+                    "Helvetica Neue",
+                    "Arial",
+                    "Noto Sans",
+                    "Apple Color Emoji",
+                    "Segoe UI Emoji",
+                    "Segoe UI Symbol",
+                    "Noto Color Emoji",
+                ],
+            },
+        },
+    },
+    plugins: [],
+};
